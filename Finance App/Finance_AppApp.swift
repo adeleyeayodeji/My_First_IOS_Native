442833f966d6e33193ea7d42c4868bb0da2137ff
Adeleye Ayodeji <biodunhi@gmail.com>
2023-07-25 20:49:15 +0100

    Initial Commit

---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Adeleye Ayodeji on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
